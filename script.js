8bc2d74a7cbf8d1b03bd65857e390c155b3a85d3
Ciara Cooper <cccooper1988@gmail.com>
2023-05-23 00:39:18 -0400

    commit to github

---
+++ script.js
@@ -0,0 +1,45 @@
+//Primitive Data Types:
+//Strings, Numbers, Boolean, null, undefined
+
+// const name = 'John';
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
+
+//Concatenation
+//console.log('My name is ' + name + ' and I am ' + age)
+
+//Template Literals
+//console.log(`My name is ${name} and I am ${age}`);
+
+//const s = 'Heyy thereee, howww are u, whoooo are u, ';
+//console.log(s.length); -The .length is getting the number of characters
+//console.log(s.substring(0, 5).toUpperCase()); //.substring is getting a 
+//portion of the string
+//console.log(s.split(', '));//split method comes in handy you can split the values
+//to an array, then you can add it to database, etc..
+
+//Arrays- Varibles that hold mulitple values
+//There are a couple ways to create arrays:
+// Array constructor:
+//const numbers = new Array(1,2,3,4,5);
+//console.log(numbers);
+
+//const fruits = ['apples', 'oranges', 'kiwi', 'pears', 'banana'];
+//console.log(fruits[0]); //will get apples from the array
+//fruits[5] = 'grapes'//this is adding another value in the end of array
+//console.log(fruits);
+
+//fruits.push('mangos');//push method allows us to add to the array
+//without knowing the amount of values in the array
+//fruits.unshift('strawberries');//The unshift method allows us to 
+//add the very beginning of the array
+
+//fruits.pop();
+//console.log(fruits)//Pop method is removing the lest value of the array
+ 
+//Object Literals- Key Value pairs
+
